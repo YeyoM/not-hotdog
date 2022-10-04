11a96fb183317819bfb5b58c05a9fa69ad20ddc3
YeyoM <yeyomoreno2003@hotmail.com>
2022-10-04 14:23:05 -0500

    working on home page

--- components/homeComponent/homeComponent.js
+++ components/homeComponent/homeComponent.js
@@ -3,8 +3,12 @@ import { Fragment } from 'react'
 export default function HomeComponent() {
   return (
     <Fragment>
-      <h1 className='text-xl font-bold underline'>Not HotDog?</h1>
-      <p>holaaa</p>
+      <h1 className='text-5xl font-bold'>Not HotDog?</h1>
+      <ul>
+        <li>Start camera</li>
+        <li>Last picture</li>
+        <li>My account</li>
+      </ul>
     </Fragment>
   )
 }
