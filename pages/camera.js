245a10b9eb488301747693d471b12a182bb6885a
YeyoM <yeyomoreno2003@hotmail.com>
2022-10-09 21:56:00 -0500

    adding the functionalities

--- pages/camera.js
+++ pages/camera.js
@@ -1,4 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState, useRef } from 'react'
+import Image from 'next/image'
+import React from 'react'
 import Webcam from "react-webcam"
 
 export default function Camera() {
@@ -6,27 +8,44 @@ export default function Camera() {
   const [videoConstraints, setVideoConstraints] = useState({
     width: 1280,
     height: 720,
-    facingMode: "user"
+    facingMode: "user",
   })
+
+  const [imageSrc, setImageSrc] = useState(null)
+
+  const webcamRef = useRef(null)
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot()
+    setImageSrc(imageSrc)
+    console.log(imageSrc)
+  }, [webcamRef, setImageSrc])
 
   return (
     <div className='flex flex-row h-screen w-full items-center justify-center bg'>
       <div className="absolute -z-10 text-center w-full ">
         <Webcam 
           className='h-full w-full'
-          videoConstraints={videoConstraints} />
+          mirrored={true}
+          screenshotFormat="image/jpeg"
+          audio={false}
+          videoConstraints={videoConstraints} 
+          ref={webcamRef}
+        />
       </div>
       <div className='block fixed h-24 inset-x-0 bottom-0 z-10 bg-black'>
         <div className='flex justify-center h-full items-center'>
           <div className='justify-center inline-block text-center text-white z-10'>
-            <svg width="72px" height="72px" viewBox="0 0 72 72" id="emoji" xmlns="http://www.w3.org/2000/svg">
-              <g id="color">
-                <circle cx="255" cy="255" r="255"/>
-              </g>
-              <g id="line">
-                <circle cx="36" cy="36.0001" r="28" fill="currentColor" stroke="#000" stroke-linejoin="round" stroke-width="2"/>
-              </g>
-            </svg>
+            <button className='rounded-full' onClick={capture}>
+              <svg width="72px" height="72px" viewBox="0 0 72 72" id="emoji" xmlns="http://www.w3.org/2000/svg">
+                <g id="color">
+                  <circle cx="255" cy="255" r="255"/>
+                </g>
+                <g id="line">
+                  <circle cx="36" cy="36.0001" r="28" fill="currentColor" stroke="#000" stroke-linejoin="round" stroke-width="2"/>
+                </g>
+              </svg>
+            </button>
           </div>
           <div className='justify-center inline-block text-center items-center'>
             <input type='checkbox' id='active' className='hidden' />
@@ -40,7 +59,13 @@ export default function Camera() {
               </svg>
             </label>
             <div className="results fixed top-0 right-[-100%] h-full w-full transition-all duration-500 ease-in-out items-center">
-              <p>holaaa</p>
+              <div className='absolute top-[10%] self-center h-auto justify-center flex flex-col text-black p-4 bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-black sm:w-11/12 sm:left-[4.16%] xl:left-1/4 xl:w-1/2'>
+                {
+                  imageSrc && (
+                    <Image src={imageSrc} width={1280} height={720} alt="Your picture"/>
+                  )
+                }
+              </div>
               <div className='block fixed h-24 inset-x-0 bottom-0 z-10 bg-black'>
               </div>
             </div>
