568782d36635e06cece22d60166f3ac9b92f00ea
YeyoM <yeyomoreno2003@hotmail.com>
2022-10-11 10:08:20 -0500

    still working on ui

--- pages/camera.js
+++ pages/camera.js
@@ -67,7 +67,7 @@ export default function Camera() {
         <div className='flex justify-center h-full items-center'>
           {
             isMobile
-            ? <button className="menu-btn absolute left-5 top-6 z-30 text-xl  cursor-pointer transition-all duration-500 ease-in-out text-white items-center" onClick={changeCamera}>
+            ? <button className="menu-btn absolute left-5 top-6 z-0 text-xl  cursor-pointer transition-all duration-500 ease-in-out text-white items-center" onClick={changeCamera}>
                 <svg className="fill-white" width="50px" height="50px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
                   <title>ionicons-v5-e</title>
                   <path d="M432,144H373c-3,0-6.72-1.94-9.62-5L337.44,98.06a15.52,15.52,0,0,0-1.37-1.85C327.11,85.76,315,80,302,80H210c-13,0-25.11,5.76-34.07,16.21a15.52,15.52,0,0,0-1.37,1.85l-25.94,41c-2.22,2.42-5.34,5-8.62,5v-8a16,16,0,0,0-16-16H100a16,16,0,0,0-16,16v8H80a48.05,48.05,0,0,0-48,48V384a48.05,48.05,0,0,0,48,48H432a48.05,48.05,0,0,0,48-48V192A48.05,48.05,0,0,0,432,144ZM316.84,346.3a96.06,96.06,0,0,1-155.66-59.18,16,16,0,0,1-16.49-26.43l20-20a16,16,0,0,1,22.62,0l20,20A16,16,0,0,1,196,288a17.31,17.31,0,0,1-2-.14,64.07,64.07,0,0,0,102.66,33.63,16,16,0,1,1,20.21,24.81Zm50.47-63-20,20a16,16,0,0,1-22.62,0l-20-20a16,16,0,0,1,13.09-27.2A64,64,0,0,0,215,222.64,16,16,0,1,1,194.61,198a96,96,0,0,1,156,59,16,16,0,0,1,16.72,26.35Z"/>
@@ -98,8 +98,8 @@ export default function Camera() {
                 </g>
               </svg>
             </label>
-            <div className="results fixed flex flex-col justify-center overflow-hidden top-0 right-[-100%] h-full w-full transition-all duration-500 ease-in-out items-center">
-              <div className='h-fit md:p-12 sm:p-4 max-w-2xl justify-center overflow-hidden flex flex-col text-black bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-black'>
+            <div className="results fixed flex flex-col justify-center overflow-hidden top-0 right-[-100%] h-screen w-full transition-all duration-500 ease-in-out items-center">
+              <div className='h-fit md:p-12 sm:p-4 xl:top-0 top-8 max-w-2xl justify-center overflow-hidden flex flex-col text-black bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-black'>
                 <h2 className="text-4xl p-4 mb-6 font-bold mt-6">
                   {imageSrc ? 'Is your picture a Hot Dog?' : 'No Photo Taken'}
                 </h2>
@@ -107,7 +107,7 @@ export default function Camera() {
                   {
                     imageSrc
                     // eslint-disable-next-line @next/next/no-img-element
-                    ? <img src={imageSrc} className='h-full w-full object-scale-down' alt=""/>
+                    ? <img src={imageSrc} className='object-scale-down' alt=""/>
                     : null
                   }
                 </div>
